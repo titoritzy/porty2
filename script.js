682cb78dcb725847a6203ffb86c06c83cc591576
titoritzy <101949169+titoritzy@users.noreply.github.com>
2024-04-09 12:05:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 $(document).ready(function() {
-    
-  $('#menu').click(function(){
-      
-      $(this).toggleClass('fa-times');
-      $('header').toggleClass('toggle');
- });
+    // Menu Toggle Function
+    $('#menu').click(function(){
+        $('header').toggleClass('toggle');
+    });
 
- $(window).on('scroll load',function(){
-    
-    $('menu').removeClass('fa-times');
-    $(header).removeClass('toggle');
+    // Window Scroll Function
+    $(window).on('scroll', function(){
+        $('header').removeClass('toggle');
 
-   if($(window).scrollTop()>0){
-     $('.top').show();
-   }else{
-    $('.top').hide();
-   }
-   
+        if ($(window).scrollTop() > 0) {
+            $('.top').show();
+        } else {
+            $('.top').hide();
+        }
+    });
 
-
-  });
+    // Smooth Scrolling to Anchors
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault();
         
-  $('a[href*="#"').on('click',function(e){
-   
-    e.preventDefault();
-    
-    $('html, body').animate({
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        }, 500, 'linear');
+    });
 
-        scrollTop: $($(this).attr('href')).offset().top,
-    },
-    500,
-    'linear'  
-    );
-  });
- });
+    // Form Submission Handling
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        // Simulate submission success (replace this with your actual submission logic)
+        // For demonstration purposes, we'll just show a message after 2 seconds
+        setTimeout(function() {
+            $('#submission-message').text('Submission successful!');
+        }, 2000);
+    });
+});
